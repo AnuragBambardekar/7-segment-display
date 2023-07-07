8eba71b318bf9f890619728fa33e1a0aeb6d23bf
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-07-07 14:50:38 -0400

    added multiple 7 segment displays in sketch2.js and finished sketch.js script

--- sketch.js
+++ sketch.js
@@ -1,38 +1,60 @@
+let nums = [0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B];
+let index = 0;
+
 function setup() {
     createCanvas(400,400);
+    frameRate(1);
 }
 
 function draw() {
-    background(220);
-    sevenSegment();
+    background(0);
+    sevenSegment(nums[index]);
+    index = (index+1) % nums.length;
 }
 
-function sevenSegment() {
+
+function getColor(val, shift) {
+    let r = 0;
+    let g = 255;
+    let b = 0;
+    let a = 255 * ((val >> shift) & 1);
+    return color(r,g,b,a);
+}
+
+function sevenSegment(val) {
     push();
     stroke(0);
+    // colorMode(RGB, 1)
     noFill();
 
     // fill(40);
     // A
-    rect(60, 20, 78, 18); // x, y, w, h - top
+    fill(getColor(val, 6));
+    rect(60, 20, 78, 18, 10, 10, 10, 10); // x, y, w, h - top
     
     // B
-    rect(140, 40, 18, 98); // u-right
+    fill(getColor(val, 5));
+    rect(140, 40, 18, 98, 10, 10, 10, 10); // u-right
 
     // C
-    rect(140, 160, 18, 98); // b-right
+    fill(getColor(val, 4));
+    rect(140, 160, 18, 98, 10, 10, 10, 10); // b-right
 
     // D
-    rect(60, 260, 78, 18); // bottom
+    fill(getColor(val, 3));
+    rect(60, 260, 78, 18, 10, 10, 10, 10); // bottom
 
     // E
-    rect(40, 160, 18, 98); // b-left
+    fill(getColor(val, 2));
+    rect(40, 160, 18, 98, 10, 10, 10, 10); // b-left
     
     // F
-    rect(40, 40, 18, 98); // u-left
+    fill(getColor(val, 1));
+    rect(40, 40, 18, 98, 10, 10, 10, 10); // u-left
     
     // G
-    rect(60, 140, 78, 18); // middle
+    fill(getColor(val, 0));
+    rect(60, 140, 78, 18, 10, 10, 10, 10); // middle
 
     pop();
 }
